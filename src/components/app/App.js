463a15af51defed7045dbfc6d584d85180e76f33
Luemigues <lulimgues@hotmail.com>
2021-02-20 19:19:29 +0100

    react app set up

---
+++ src/components/app/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Portfolio Luemigues
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
