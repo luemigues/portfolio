47174cb9bcafba0ede6de789b015c2f2fdbcbffa
luciaMetodika <lucia@metodika.com>
2021-11-28 20:42:00 +0100

    update about me section

--- src/components/sections/aboutme/profile/profile.js
+++ src/components/sections/aboutme/profile/profile.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import {Photo, Name, Title, Description, ProfileContainer, TextContainer} from './profile-styling.js';
-import profilePhoto from '../../../../assets/CV-Migues.jpg';
+import React from "react";
+import {
+  Photo,
+  Name,
+  Title,
+  Description,
+  ProfileContainer,
+  TextContainer,
+} from "./profile-styling.js";
+import profilePhoto from "../../../../assets/CV-Migues.jpg";
 
-function Profile(){
-    return (
-        <ProfileContainer>
-            <Photo src={profilePhoto}/>
-            <TextContainer>
-                <Name>
-                    Lucia Eugenia Migues
-                </Name>
-                <Title>
-                    JR Frontend Developer
-                </Title>
-                <Description>
-                    I recently moved to Stockholm, Sweden and I am interested in taking my first steps in the world of web development, with knowledge in HTML, CSS, JavaScript, React and Git.<br/>
-                    <br/> I consider myself a proactive and eager to learn person. Always trying to learn, improve and look for new challenges.<br/>
-                    <br/>My most recent work experience was as Marketing Leader, leading the design team. In this position, I have managed to enhance my soft skills by dealing with clients and continuous teamwork. I have also strengthened my experience in designing, including tools such as the Adobe package into my day to day.
-                </Description>
-            </TextContainer>
-        </ProfileContainer>
-    );
+function Profile() {
+  return (
+    <ProfileContainer>
+      <Photo src={profilePhoto} />
+      <TextContainer>
+        <Name>Lucia Eugenia Migues</Name>
+        <Title>Frontend Developer JR</Title>
+        <Description>
+          I recently moved to Stockholm, Sweden and I am taking my
+          first steps in the world of web development, with knowledge in Javascript, React, HTML,
+          CSS, Node.js and Git.
+          <br />
+          <br /> I consider myself a proactive and eager to learn person. Always
+          trying to learn, improve and look for new challenges.
+          <br />
+          <br /> My goal is to combine my passion for coding and design to
+          become a skilled frontend developer, creating clean and efficient code
+          that gives users the best experience.
+          <br />
+          <br />I am currently working as Software Developer, focusing on
+          improving my coding skills, problem solving and decision making in
+          order to prioritize the development of the product and the client’s
+          needs.
+        </Description>
+      </TextContainer>
+    </ProfileContainer>
+  );
 }
 
-export default Profile
+export default Profile;
