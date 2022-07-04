88feb9d12bbba2ee590340fd7a595fb986fd4e6b
luemigues <luemigues@gmail.com>
2022-07-04 17:42:42 +0200

    add tv watcher project

--- src/components/sections/aboutme/skills/skills.js
+++ src/components/sections/aboutme/skills/skills.js
@@ -1,36 +1,50 @@
-import React from 'react';
-import {SkillsContainer, DevelopmentSkills, DesignSkills} from './skills.-styling';
-import Skill from './skill-item/skill-item.js';
-import {styleSkills} from '../../../../utils/helperfunctions';
+import React from "react";
+import {
+  SkillsContainer,
+  DevelopmentSkills,
+  DesignSkills,
+} from "./skills-styling";
+import Skill from "./skill-item/skill-item.js";
+import { styleSkills } from "../../../../utils/helperfunctions";
 
-import {Javascript} from '@styled-icons/simple-icons/Javascript';
-import {ReactLogo} from '@styled-icons/simple-icons/ReactLogo';
-import {Html5} from '@styled-icons/simple-icons/Html5';
-import {Css3} from '@styled-icons/simple-icons/Css3';
-import {NodeDotJs} from '@styled-icons/simple-icons/NodeDotJs';
-import {Git} from '@styled-icons/simple-icons/Git';
-import {Sass} from '@styled-icons/fa-brands/Sass';
-import {Adobexd} from '@styled-icons/simple-icons/Adobexd';
-import {Adobephotoshop} from '@styled-icons/simple-icons/Adobephotoshop';
-import {Adobeillustrator} from '@styled-icons/simple-icons/Adobeillustrator';
-import {Adobepremierepro} from '@styled-icons/simple-icons/Adobepremierepro';
-import {Adobelightroom} from '@styled-icons/simple-icons/Adobelightroom';
+import { Javascript } from "@styled-icons/simple-icons/Javascript";
+import { ReactLogo } from "@styled-icons/simple-icons/ReactLogo";
+import { Html5 } from "@styled-icons/simple-icons/Html5";
+import { Css3 } from "@styled-icons/simple-icons/Css3";
+import { NodeDotJs } from "@styled-icons/simple-icons/NodeDotJs";
+import { Git } from "@styled-icons/simple-icons/Git";
+import { Sass } from "@styled-icons/fa-brands/Sass";
+import { Adobexd } from "@styled-icons/simple-icons/Adobexd";
+import { Adobephotoshop } from "@styled-icons/simple-icons/Adobephotoshop";
+import { Adobeillustrator } from "@styled-icons/simple-icons/Adobeillustrator";
+import { Adobepremierepro } from "@styled-icons/simple-icons/Adobepremierepro";
+import { Adobelightroom } from "@styled-icons/simple-icons/Adobelightroom";
 
-const devSkills = [{Icon: Javascript ,name: 'Javascript' }, {Icon: ReactLogo ,name: 'React JS' }, {Icon: Html5 ,name: 'HTML5' }, {Icon: Css3 ,name: 'Css' },{Icon: NodeDotJs ,name: 'Node JS' },{Icon: Git ,name: 'Git' },{Icon: Sass ,name: 'Sass' }];
-const designSkills = [ {Icon: Adobexd ,name: 'Adobe XD' }, {Icon: Adobephotoshop ,name: 'Photoshop' }, {Icon: Adobeillustrator ,name: 'Illustrator' },{Icon: Adobepremierepro ,name: 'Premiere' }, {Icon: Adobelightroom ,name: 'Lightroom' }]
+const devSkills = [
+  { Icon: Javascript, name: "Javascript" },
+  { Icon: ReactLogo, name: "React JS" },
+  { Icon: Html5, name: "HTML5" },
+  { Icon: Css3, name: "Css" },
+  { Icon: NodeDotJs, name: "Node JS" },
+  { Icon: Git, name: "Git" },
+  { Icon: Sass, name: "Sass" },
+];
 
-function Skills(){
+const designSkills = [
+  { Icon: Adobexd, name: "Adobe XD" },
+  { Icon: Adobephotoshop, name: "Photoshop" },
+  { Icon: Adobeillustrator, name: "Illustrator" },
+  { Icon: Adobepremierepro, name: "Premiere" },
+  { Icon: Adobelightroom, name: "Lightroom" },
+];
 
-    return(
-        <SkillsContainer>
-            <DevelopmentSkills>
-                {styleSkills(devSkills, Skill)}
-            </DevelopmentSkills>
-            <DesignSkills>
-                {styleSkills(designSkills, Skill)}
-            </DesignSkills>
-        </SkillsContainer>
-    )
+function Skills() {
+  return (
+    <SkillsContainer>
+      <DevelopmentSkills>{styleSkills(devSkills, Skill)}</DevelopmentSkills>
+      <DesignSkills>{styleSkills(designSkills, Skill)}</DesignSkills>
+    </SkillsContainer>
+  );
 }
 
 export default Skills;
--- src/utils/sitesInfo.js
+++ src/utils/sitesInfo.js
@@ -1,54 +1,51 @@
-import blueDotPic from '../assets/Palebluedot.PNG';
-import ravenousPic from '../assets/ravenous.PNG';
-import jammingPic from '../assets/Jamming.PNG';
-import gifosPic from '../assets/gifos.PNG';
-import delilahPic from '../assets/delilah.PNG';
-import portfolioPic from '../assets/portfolio.PNG';
+import tvWatcherPic from "../assets/tvwatcher.PNG";
+import ravenousPic from "../assets/ravenous.PNG";
+import jammingPic from "../assets/Jamming.PNG";
+import gifosPic from "../assets/gifos.PNG";
+import delilahPic from "../assets/delilah.PNG";
+import portfolioPic from "../assets/portfolio.PNG";
 
-
-const blueDot = {
-    img: blueDotPic,
-    title: "Pale Blue Dot",
-    github: "https://github.com/luemigues/Poscast",
-    website: "https://luemigues-podcast.netlify.app/"
-}
+const tvWatcher = {
+  img: tvWatcherPic,
+  title: "TV Watcher",
+  github: "https://github.com/luemigues/tvWatcher",
+  website: "https://tv-watcher.netlify.app/",
+};
 
 const ravenous = {
-    img: ravenousPic,
-    title: "Ravenous",
-    github: "https://github.com/luemigues/Ravenous",
-    website: "https://luemigues-ravenous.netlify.app/"
-}
+  img: ravenousPic,
+  title: "Ravenous",
+  github: "https://github.com/luemigues/Ravenous",
+  website: "https://luemigues-ravenous.netlify.app/",
+};
 
 const jamming = {
-    img: jammingPic,
-    title: "Jamming",
-    github: "https://github.com/luemigues/jammingApp",
-    website: "https://luemigues-jamming.netlify.app/"
-}
+  img: jammingPic,
+  title: "Jamming",
+  github: "https://github.com/luemigues/jammingApp",
+  website: "https://luemigues-jamming.netlify.app/",
+};
 
 const gifos = {
-    img: gifosPic,
-    title: "Gifos",
-    github: "https://github.com/luemigues/gifOS",
-    website: "https://luemigues-gifos.netlify.app/"
-}
+  img: gifosPic,
+  title: "Gifos",
+  github: "https://github.com/luemigues/gifOS",
+  website: "https://luemigues-gifos.netlify.app/",
+};
 
 const delilah = {
-    img: delilahPic,
-    title: "Delilah Resto API Rest",
-    github: "https://github.com/luemigues/delilah-resto",
-    website: "https://documenter.getpostman.com/view/12198401/TW74k5fZ"
-}
+  img: delilahPic,
+  title: "Delilah Resto API Rest",
+  github: "https://github.com/luemigues/delilah-resto",
+  website: "https://documenter.getpostman.com/view/12198401/TW74k5fZ",
+};
 
 const portfolio = {
-    img: portfolioPic,
-    title: "Portfolio",
-    github: "https://github.com/luemigues/portfolio",
-}
+  img: portfolioPic,
+  title: "Portfolio",
+  github: "https://github.com/luemigues/portfolio",
+};
 
-
-const sites = [gifos, ravenous, jamming, delilah, blueDot, portfolio];
-
+const sites = [portfolio, tvWatcher, delilah, ravenous, jamming, gifos];
 
 export default sites;
